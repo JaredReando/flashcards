b86a9be8f208a70ec2e5d6deb6b56b1647a159e2
Daniel Bennett and Jared Reando <jared@>
2018-10-16 15:25:40 -0700

    Add colors and position changes.

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,98 @@ $(function(){
     $("#p2").toggle();
   });
 });
+
+$(function(){
+  $("#clickhere3").click(function(){
+    $("#p3").toggle();
+  });
+});
+
+$(function(){
+  $("#clickhere4").click(function(){
+    $("#p4").toggle();
+  });
+});
+
+$(function(){
+  $("#clickhere5").click(function(){
+    $("#p5").toggle();
+  });
+});
+
+$(function(){
+  $("#clickhere6").click(function(){
+    $("#p6").toggle();
+  });
+});
+
+$(function(){
+  $("#clickhere7").click(function(){
+    $("#p7").toggle();
+  });
+});
+
+$(function(){
+  $("#clickhere8").click(function(){
+    $("#p8").toggle();
+  });
+});
+
+$(function(){
+  $("#clickhere9").click(function(){
+    $("#p9").toggle();
+  });
+});
+
+$(function(){
+  $("#clickhere10").click(function(){
+    $("#p10").toggle();
+  });
+});
+
+$(function(){
+  $("#clickhere11").click(function(){
+    $("#p11").toggle();
+  });
+});
+
+$(function(){
+  $("#clickhere12").click(function(){
+    $("#p12").toggle();
+  });
+});
+
+$(function(){
+  $("#clickhere13").click(function(){
+    $("#p13").toggle();
+  });
+});
+$(function(){
+  $("#clickhere14").click(function(){
+    $("#p14").toggle();
+  });
+});
+
+$(function(){
+  $("#clickhere15").click(function(){
+    $("#p15").toggle();
+  });
+});
+
+$(function(){
+  $("#clickhere16").click(function(){
+    $("#p16").toggle();
+  });
+});
+
+$(function(){
+  $("#clickhere17").click(function(){
+    $("#p17").toggle();
+  });
+});
+
+$(function(){
+  $("#clickhere18").click(function(){
+    $("#p18").toggle();
+  });
+});
